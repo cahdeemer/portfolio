05cc96435be936a32d7c9745e435a0ec4f6b9c06
Christina Deemer <cahdeemer@gmail.com>
2017-07-04 09:15:25 -0400

    inc bootstrap nav

--- header.php
+++ header.php
@@ -24,32 +24,43 @@
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'chd_portfolio' ); ?></a>
 
 	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) : ?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<?php else : ?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-			<?php
-			endif;
+		<nav id="site-navigation" class="navbar navbar-default main-navigation" >
+  			<div class="container-fluid">
+    		<!-- Brand and toggle get grouped for better mobile display -->
+    			<div class="navbar-header">
+      				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        				<span class="sr-only">Toggle navigation</span>
+        				<span class="icon-bar"></span>
+        				<span class="icon-bar"></span>
+        				<span class="icon-bar"></span>
+      				</button>
 
-			$description = get_bloginfo( 'description', 'display' );
-			if ( $description || is_customize_preview() ) : ?>
-				<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
-			<?php
-			endif; ?>
-		</div><!-- .site-branding -->
+      				<div class="site-branding">
+						<?php if ( is_front_page() && is_home() ) : ?>
 
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'chd_portfolio' ); ?></button>
-			<?php
-				wp_nav_menu( array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
-				) );
-			?>
-		</nav><!-- #site-navigation -->
+							<h1 class="site-title"><a class="navbar-brand" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">Christina Deemer</a></h1>
+						
+						<?php else : ?>
+							
+							<p class="site-title"><a class="navbar-brand" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+						
+						<?php endif; ?>
+					</div><!-- .site-branding -->
+    			</div>
+
+    		<!-- Collect the nav links, forms, and other content for toggling -->
+    		
+    			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      
+		     		<ul class="nav navbar-nav navbar-right">
+		        		<li><a href="#">About</a></li>
+		        		<li><a href="#">Projects</a></li>
+		        		<li><a href="#">Blog</a></li>
+		      		</ul>
+
+    			</div><!-- /.navbar-collapse -->
+  			</div><!-- /.container-fluid -->
+		</nav>
 	</header><!-- #masthead -->
 
 	<div id="content" class="site-content">
--- inc/template-tags.php
+++ inc/template-tags.php
@@ -14,10 +14,10 @@ if ( ! function_exists( 'chd_portfolio_posted_on' ) ) :
 function chd_portfolio_posted_on() {
 	$timechd_portfoliotring = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
 	if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
-		$timechd_portfoliotring = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
+		$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
 	}
 
-	$timechd_portfoliotring = sprintf( $timechd_portfoliotring,
+	$time_string = sprintf( $time_string,
 		esc_attr( get_the_date( 'c' ) ),
 		esc_html( get_the_date() ),
 		esc_attr( get_the_modified_date( 'c' ) ),
@@ -27,7 +27,7 @@ function chd_portfolio_posted_on() {
 	$posted_on = sprintf(
 		/* translators: %s: post date. */
 		esc_html_x( 'Posted on %s', 'post date', 'chd_portfolio' ),
-		'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $timechd_portfoliotring . '</a>'
+		'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
 	);
 
 	$byline = sprintf(
