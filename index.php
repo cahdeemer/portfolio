9ef235951b3580b69fd240fc5705a8c9f9d2501d
Christina Deemer <cahdeemer@gmail.com>
2017-07-16 12:27:12 -0400

    blog index, hero fixes

--- index.php
+++ index.php
@@ -17,36 +17,118 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
 
-		<?php
-		if ( have_posts() ) :
+		<section class="jumbotron">
+  				<div class="container">
+  					
+  					<div class="hero__background-single"></div>
+  					
+  					<div class="hero__callout-center">
+  						<h6>maybe a subheading</h6>
+  						<h1 class="entry-title">blog</h1>
+  						<a href="">here's a thing</a>
+  					</div>
+   
+ 				</div>
+		</section>
+		<article class="container-fluid">
+			<section class="section__featured row">
+				<h2 class="section__title-center">featured post</h2>
+				<div class="col-sm-10 col-sm-offset-1">
+					<div class="col-sm-4">
+						<img src="http://rs150.pbsrc.com/albums/s85/michelleNpete/BaBas/awesome-beautiful-blue-eyes-cat-cute-Favimcom-110476.jpg~c200">
 
-			if ( is_home() && ! is_front_page() ) : ?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
+					</div>
+					<div class="col-sm-8">
+						<h5 class="featured-date">January 1, 2018</h5>
+						<h2 class="featured-title">post title</h2>
+						<p class="featured-excerpt">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec pretium pharetra purus vitae mattis. Vestibulum et malesuada nisl. Suspendisse faucibus felis sit amet lacus condimentum dictum. Nam aliquam molestie pretium. Nam mi diam, efficitur non pharetra vitae, consequat ac turpis. </p>
+					</div>
+				</div>
 
-			<?php
-			endif;
 
-			/* Start the Loop */
-			while ( have_posts() ) : the_post();
+			</section>
 
-				/*
-				 * Include the Post-Format-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_format() );
+			<section class="section__posts row">
+				<h2 class="section__title-left">most recent posts</h2>
+				<div class="col-sm-10 col-sm-offset-1 posts__wrapper">
+					  	<a class="project__single post__preview">
+					  	
+						  	<img src="https://static.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg" alt="cat placeholder">
+						  	<h5>January 1, 2018</h5>
+						  	<h3>Title of Post</h3>
+						  	<div class="project__overlay">
+						  		<h5>Tag 1</h5>
+						  	</div>
+					  	</a>
+					  	<a class="project__single post__preview">
+					  	
+						  	<img src="https://static.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg" alt="cat placeholder">
+						  	<h5>Spring 2015</h5>
+						  	<h3>Title of Project</h3>
+						  	<div class="project__overlay">
+						  		<h5>Tag 1 </h5>
+						  		<h5>Tag 2</h5>
+						  	</div>
+					  	</a>
 
-			endwhile;
+					  	<a class="project__single post__preview">
+					  	
+						  	<img src="https://static.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg" alt="cat placeholder">
+						  	<h5>Spring 2015</h5>
+						  	<h3>Title of Project</h3>
+						  	<div class="project__overlay">
+						  		<h5>Tag 1</h5>
+						  		<h5>Tag 2</h5>
+						  		<h5>Tag 3</h5>
+						  	</div>
+					  	</a>
+					  	<a class="project__single post__preview">
+					  	
+						  	<img src="https://static.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg" alt="cat placeholder">
+						  	<h5>Spring 2015</h5>
+						  	<h3>Title of Project</h3>
+						  	<div class="project__overlay">
+						  		<h5>Tag 1</h5>
+						  		<h5>Tag 2</h5>
+						  		<h5>Tag 3</h5>
+						  		<h5>Tag 4</h5>
+						  	</div>
+					  	</a>
+				</div>
+			</section>
 
-			the_posts_navigation();
+			<section class="section__tags row">
+				<h2 class="section__title-center">all tags</h2>
+				<div class="col-sm-10 col-sm-offset-1 tag__wrapper">
+					<a class="btn btn-default" href="">Tag 1</a>
+					<a class="btn btn-default" href="">Tag 2</a>
+					<a class="btn btn-default" href="">Long Tag Name</a>
+					<a class="btn btn-default" href="">Tag 4</a>
+					<a class="btn btn-default" href="">Tag 1</a>
+					<a class="btn btn-default" href="">Tag 2</a>
+					<a class="btn btn-default" href="">Tag 3</a>
+					<a class="btn btn-default" href="">Even Longer Tag Name</a>
+					<a class="btn btn-default" href="">Tag 1</a>
+					<a class="btn btn-default" href="">Tag 2</a>
+					<a class="btn btn-default" href="">Tag 3</a>
+					<a class="btn btn-default" href="">Tag 4</a>
+				</div>
+			</section>
 
-		else :
 
-			get_template_part( 'template-parts/content', 'none' );
 
-		endif; ?>
+		</article>
+
+		
+
+
+
+
+
+
+
+
+		
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
